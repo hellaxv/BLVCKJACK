751cbf34eb5ead75cb3f5ee3fd10539302a7c82d
Lenovo <Lenovo@LAPTOP-47RANI4K>
2021-11-13 04:35:28 +0900

    Добавьте файлы проекта.

---
+++ BLVCKJ4CK/BLVCKJ4CK.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include "blvckj4ck.h"
+
+int main()
+{
+    std::vector<std::string> playerNames = { "Smith" };
+    Game* game = new Game(playerNames);
+    bool isInProgress = true;
+    while (isInProgress)
+    {
+        game->Play();
+        std::cout << "\nPlay another game? (Y/N): ";
+        bool formatCheck = true;
+        std::string input;
+        std::getline(std::cin, input);
+        if (std::cin && (input == "N" || input == "n")) isInProgress = false;
+        else if (std::cin && (input == "Y" || input == "y")) isInProgress = true;
+        else
+        {
+            //system("cls");
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            std::cout << "Incorrect input!";
+        }
+    }
+    delete game;
+    return 0;
+}
+
